d8d554886e1048e836109eb969c376ce0609117d
Lloyd Evans <contact@lloydevans.co.uk>
2020-08-18 15:03:14 +0100

    Update build-readme.js

--- build/build-readme.js
+++ build/build-readme.js
@@ -1,22 +1,22 @@
 const { readFile, writeFile } = require("fs").promises;
 
 (async function () {
-  let docs, intro, readme;
+  let docs, intro;
 
   try {
     docs = (await readFile("docs/md/README.md")).toString();
     intro = (await readFile("docs/HEADER.md")).toString();
+
+    let insertIdx = docs.indexOf("## Index");
+    let head = docs.slice(0, insertIdx);
+    let page = docs.slice(insertIdx);
+    head += "\n";
+    head += intro;
+    head += "\n";
+    head += page;
+
+    await writeFile("README.md", head);
   } catch (error) {
     console.log(error);
   }
-
-  let insertIdx = docs.indexOf("## Index");
-  let head = docs.slice(0, insertIdx);
-  let page = docs.slice(insertIdx);
-  head += "\n";
-  head += intro;
-  head += "\n";
-  head += page;
-
-  await writeFile("README.md", head);
 })();
